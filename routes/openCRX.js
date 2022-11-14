925dae2af50635659c9cfb16e65e774b857a2668
youneszayakh <younes.zayakh@smail.inf.h-brs.de>
2022-11-14 22:26:02 +0100

    provided:  - product, salesOrder endpoints (requests)

--- routes/openCRX.js
+++ routes/openCRX.js
@@ -16,11 +16,41 @@ const config = {
     httpsAgent: httpsAgent, // this was missing (for what is this)?
     auth: credentials,
 };
-router.get('/', async (req, res, next) => {
+
+router.get('/account', async (req, res, next) => {
 
     const contacts = await axios.get(`${baseUrl}/org.opencrx.kernel.account1/provider/CRX/segment/Standard/account`, config);
-    const accounts = contacts.data.objects;
-    res.send(accounts)
+    const accounts = contacts.data.objects
+
+    var salesMenArray = []
+    var customersArray = []
+
+    accounts.map((value) => {
+        if (value['@type'].endsWith('LegalEntity')) {
+            customersArray.push(value);
+        }
+        if (value['jobTitle'] && value['jobTitle'] === 'Senior Salesman') {
+            salesMenArray.push(value);
+        }
+    });
+
+    res.send([customersArray,salesMenArray])
+});
+
+router.get('/product', async (req, res, next) => {
+
+    const response = await axios.get(`${baseUrl}/org.opencrx.kernel.product1/provider/CRX/segment/Standard/product`, config);
+    const products = response.data.objects;
+
+    res.send(products)
+});
+
+router.get('/salesOrder', async (req, res, next) => {
+
+    const response = await axios.get(`${baseUrl}/org.opencrx.kernel.contract1/provider/CRX/segment/Standard/salesOrder`, config);
+    const salesOrders = response.data.objects;
+
+    res.send(salesOrders)
 });
 
 module.exports = router;
